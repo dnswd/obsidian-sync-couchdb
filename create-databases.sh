f13e7f093f57e06a08e3548d92410e820e9f97e0
Dennis A. Walangadi <dennis.walangadi@gmail.com>
2023-08-02 09:15:33 +0700

    setup obsidian livesync

--- create-databases.sh
+++ create-databases.sh
@@ -19,6 +19,15 @@ for db in "${DATABASES[@]}"; do
   curl -X PUT "$COUCHDB_URL/$db"
 done
 
-# Additional setup can be done here, such as creating views, etc.
+# Setup obsidian livesync
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/chttpd/require_valid_user" -H "Content-Type: application/json" -d '"true"' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/chttpd_auth/require_valid_user" -H "Content-Type: application/json" -d '"true"' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/httpd/WWW-Authenticate" -H "Content-Type: application/json" -d '"Basic realm=\"couchdb\""' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/httpd/enable_cors" -H "Content-Type: application/json" -d '"true"' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/chttpd/enable_cors" -H "Content-Type: application/json" -d '"true"' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/chttpd/max_http_request_size" -H "Content-Type: application/json" -d '"4294967296"' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/couchdb/max_document_size" -H "Content-Type: application/json" -d '"50000000"' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/cors/credentials" -H "Content-Type: application/json" -d '"true"' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
+curl -X PUT "$COUCHDB_URL/_node/nonode@nohost/_config/cors/origins" -H "Content-Type: application/json" -d '"app://obsidian.md,capacitor://localhost,http://localhost"' --user "${ADMIN_USER}:${ADMIN_PASSWORD}"
 
 echo "Databases created successfully."
